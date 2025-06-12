00723ef0d0e7c594ca44933d9db49aa3bf3e8169
enak04 <f20230539@hyderabad.bits-pilani.ac.in>
2025-06-12 17:49:53 +0530

    Third commit

--- Backend/wsgi.py
+++ Backend/wsgi.py
@@ -1,8 +1,13 @@
 import os
+import sys
+
+# Adds the Backend folder to sys.path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT" , 4000))
-    app.run(host = "0.0.0.0" , port = port)
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", port=port)

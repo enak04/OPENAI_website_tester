0a3b3b7bec2d0d291fdc24ee2060cb2119bbbd07
enak04 <f20230539@hyderabad.bits-pilani.ac.in>
2025-07-08 16:10:24 +0530

    Jsonify edits

--- app/routes.py
+++ app/routes.py
@@ -141,7 +141,7 @@ def chat(user_id):
         json_result = get_json_by_theme_name(selected_theme.lower())
         store_css_and_json_for_user(user_id , css_result , json_result , json_id)
         user_message =  "Edit my website theme when " + user_message
-    print("\nuser message is : " , user_message)
+    print("\nuser message is : " , user_message , "\njson_id is : " , json_id)
 
     if not user_message:
         return jsonify({"error": "No message provided"}), 400
